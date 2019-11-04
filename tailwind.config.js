07371f7bab1cb28273673155a66a427f306c1428
chr1sw <cwoehrle@gmail.com>
2019-11-04 09:37:16 -0500

    Version 1 to publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      'display': ['Staatliches', 'sans-serif'],
-    },
-    extend: {}
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+        '120': '30rem',
+        '132': '33rem',
+        '144': '36rem',
+      }
+    }
   },
   variants: {},
   plugins: []
